28983d01ce3106dffe52d9a98fcafed3541044f5
emerging-pc <115644671+emerging-pc@users.noreply.github.com>
2023-08-06 14:53:10 +0530

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,16 @@
+#PROJECT 01
+
+from googletrans import Translator
+
+def translate_text(text, target_language='es'):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+
+def main():
+    source_text = input("Enter the text to translate (in English): ")
+    translated_text = translate_text(source_text)
+    print(f"Translated text (in Spanish): {translated_text}")
+
+if __name__ == "__main__":
+    main()
